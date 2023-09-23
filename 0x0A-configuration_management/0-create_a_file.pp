a7c2a2c079db6d97835485440b3ce1778e017efc
Abdullahi <abdush504@gmail.com>
2023-09-23 12:40:23 -0700

    used puppet to create a file in /tmp

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# creates a file with the content "I love Puppet" and the owner www-data
+file { '/tmp/school':
+  ensure  => 'file',
+  content => 'I love Puppet',
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744',
+}
