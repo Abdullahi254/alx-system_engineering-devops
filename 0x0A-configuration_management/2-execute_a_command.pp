489652c65fa1ea662c905f9a3ec5e9160ed297c7
Abdullahi <abdush504@gmail.com>
2023-09-23 12:52:44 -0700

    bug fix

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,5 @@
 # kills all processes named killmenow
-exec{'killmenow':
- command => 'pkill killmenow',
- path => ['/usr/bin', '/usr/sbin',],
+exec { 'killmenow':
+  command => 'pkill killmenow',
+  path    => ['/usr/bin', '/usr/sbin',],
 }
